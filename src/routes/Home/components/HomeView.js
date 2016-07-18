ec8204e56e7447480e26aafbe1fbf7696caae941
Dave Edelhart <dave@wonderlandlabs.com>
2016-07-18 11:28:21 -0700

    final(?) resolution fixing home link

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -15,7 +15,7 @@ export const HomeView = () => (
         <p>This is entirely clientside using React and Flux. Enjoy!
         </p>
         <p><Link to="chess" activeClassName={classes.activeRoute}>
-            Let's play augmentedd Augmented Chess
+            Let's play Augmented Chess
         </Link></p>
     </div>
 );
