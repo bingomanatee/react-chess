32b2babc7b3504db93dc4d3e65d9e1faec62b6b0
Dave Edelhart <dave@wonderlandlabs.com>
2016-07-18 11:10:26 -0700

    applying for github pages

--- config/environments.js
+++ config/environments.js
@@ -23,8 +23,8 @@ export default {
     // Overrides when NODE_ENV === 'production'
     // ======================================================
     production: (config) => ({
-        compiler_public_path: '/bingomanatee/react-chess',
-        server_host: 'www.github.com', // use string 'localhost' to prevent exposure on local network
+        compiler_public_path: '/react-chess',
+        server_host: 'bingomanatee.github.com', // use string 'localhost' to prevent exposure on local network
         dir_dist: '/',
         server_port: 80,
         compiler_fail_on_warning: false,
